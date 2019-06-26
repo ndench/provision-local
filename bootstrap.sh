b63adb2440b3dc28b78d89015cd62824347492b1
Nathan Dench <ndenc2@gmail.com>
2019-06-26 20:37:58 +1000

    Add config role

--- bootstrap.sh
+++ bootstrap.sh
@@ -5,10 +5,13 @@ set -eux
 # Ask for sudo upfront
 sudo -v
 
-git clone https://github.com/ndench/provision-local.git ~/git/ndench/provision-local
 
-cd ~/git/ndench/provision-local
-git remote set-url origin git@github.com:ndench/provision-local.git
+# Clone this repo
+if [[ ! -f ~/git/ndench/provision-local ]]; then
+  git clone https://github.com/ndench/provision-local.git ~/git/ndench/provision-local
+  cd ~/git/ndench/provision-local
+  git remote set-url origin git@github.com:ndench/provision-local.git
+fi
 
 cd ~/git/ndench/provision-local
 
