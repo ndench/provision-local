32c16d78c47ae765492be043b581d69dc71fbf14
Nathan Dench <ndenc2@gmail.com>
2019-07-01 10:44:33 +1000

    Echo vault password to correct location, mach II

--- bootstrap.sh
+++ bootstrap.sh
@@ -2,20 +2,18 @@
 
 set -eux
 
-read -sp "Enter vault password: " vault_password
-
 echo "Asking for sudo password upfront so that it's cached"
 sudo -v
 
-clone_path=~/git/ndench/provision-local
-
 # Clone this repo
+clone_path=~/git/ndench/provision-local
 if [[ ! -d ${clone_path} ]]; then
   git clone https://github.com/ndench/provision-local.git ${clone_path}
+  read -sp "Enter vault password: " vault_password
+  echo ${vault_password} > ${clone_path}/.ansible_vault.pass
 fi
 
 cd ${clone_path}
-echo ${vault_password} > .ansible_vault.pass
 
 make
 
